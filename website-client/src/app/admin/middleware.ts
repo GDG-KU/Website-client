5ee877944ebd9ff0cb5a44fda9da8180c7207e51
JungJae Lee <jerrylee1516@gmail.com>
2025-03-07 22:47:54 +0900

    미들웨어추가

---
+++ website-client/src/app/admin/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+
+  if (url.pathname.startsWith('/admin')) {
+    const token = request.cookies.get('token')?.value;
+    const isCore = request.cookies.get('isCore')?.value;
+
+    if (!token || isCore !== 'true') {
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
